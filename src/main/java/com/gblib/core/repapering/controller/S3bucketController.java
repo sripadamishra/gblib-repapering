316cc61c20af98e076ec385604c3c167f6ec5fd4
Manish <manishsil@yahoo.com>
2020-09-18 14:04:57 +0530

    S3 integration
    Change-Id: I6a0ffa256432ef258b713c78810befa17b0a19df

--- src/main/java/com/gblib/core/repapering/controller/S3bucketController.java
+++ src/main/java/com/gblib/core/repapering/controller/S3bucketController.java
@@ -1,39 +1,55 @@
 package com.gblib.core.repapering.controller;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.sql.Timestamp;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
-import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 
 import com.gblib.core.repapering.global.WorkflowStageCompletionResultEnums;
 import com.gblib.core.repapering.global.WorkflowStageEnums;
 import com.gblib.core.repapering.model.Contract;
-import com.gblib.core.repapering.model.Response;
 import com.gblib.core.repapering.model.WorkflowOCR;
 import com.gblib.core.repapering.model.WorkflowScanUpload;
+import com.gblib.core.repapering.services.AmazonClient;
 import com.gblib.core.repapering.services.ContractService;
 import com.gblib.core.repapering.services.FileStorageService;
 import com.gblib.core.repapering.services.WorkflowOCRService;
 import com.gblib.core.repapering.services.WorkflowScanUploadService;
 
 @RestController
-public class FileUploadController {
+public class S3bucketController {
+//https://medium.com/oril/uploading-files-to-aws-s3-bucket-using-spring-boot-483fcb6f8646
+	private AmazonClient amazonClient;
+	
+	@Autowired
+	S3bucketController(AmazonClient amazonClient) {
+        this.amazonClient = amazonClient;
+    }
+	
 	@Autowired
     private FileStorageService fileStorageService;
-
+	
 	@Autowired
 	WorkflowScanUploadService workflowScanUploadService;
 	
@@ -42,8 +58,7 @@ public class FileUploadController {
 	
 	@Autowired
 	ContractService contractService;
-	
-    @PostMapping("/v1/uploadFile")
+	@PostMapping("/uploadFile")
     public Contract uploadFile(@RequestParam("file") MultipartFile file,@RequestParam("userid") String userid) {
         String fileName = fileStorageService.storeFile(file);
 
@@ -51,7 +66,17 @@ public class FileUploadController {
             .path("/downloadFile/")
             .path(fileName)
             .toUriString();
-                    	    	
+                
+            //Upload file to S# bucket
+        	try {
+				this.amazonClient.uploadFile(file);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.out.println("Unable to upload file in S3 bucket");
+				e.printStackTrace();
+			}
+        	
+        	
     		//Step:1 Save into ContractWorkflowScanUpload table with CompletionResult =2
     		  
     		Date createdOn = new Timestamp(System.currentTimeMillis());
@@ -92,4 +117,28 @@ public class FileUploadController {
     		return contract;
     	}
 
+    @PostMapping("/direct/uploadFile")
+    public String uploadFile(@RequestPart(value = "file") MultipartFile file) throws Exception {
+        return this.amazonClient.uploadFile(file);
+    }
+
+    @DeleteMapping("/deleteFile")
+    public String deleteFile(@RequestPart(value = "url") String fileUrl) {
+        return this.amazonClient.deleteFileFromS3Bucket(fileUrl);
+    }
+    
+    @GetMapping(value= "/download")
+    public ResponseEntity<ByteArrayResource> downloadFile(@RequestParam(value= "fileName") final String keyName) {
+        final byte[] data = amazonClient.downloadFile(keyName);
+        //writeByte(data);;
+        final ByteArrayResource resource = new ByteArrayResource(data);
+        return ResponseEntity
+                .ok()
+                .contentLength(data.length)
+                .header("Content-type", "application/octet-stream")
+                .header("Content-disposition", "attachment; filename=\"" + keyName + "\"")
+                .body(resource);
+    }
+    
+    
 }
